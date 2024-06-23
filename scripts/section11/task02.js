f1e5f941f48dedfccad175d23b2649fb365efe55
JCampo1502 <javierdavidcamposuarez15@gmail.com>
2024-06-23 16:39:45 -0500

    feat: :sparkles: Update exercise 2 section 11

--- scripts/section11/task02.js
+++ scripts/section11/task02.js
@@ -1,81 +1,162 @@
+class Currency{
+     #dollarValue = 0;
+     #amount = 0;
+     #name = null;
 
-const formulario = document.getElementById('formulario');
-const listaMoneda = document.getElementById('moneda');
-const listaCambio = document.getElementById('criptomoneda');
-const alerta = document.getElementById('principal');
-
-const fragmento = document.createDocumentFragment();
-const fragmento2 = document.createDocumentFragment();
-
-//Vector Moneda
-var moneda = ['Elige tu Moneda','Dolar','Peso Mexicano','Peso Colombiano','Euros','Bolivar'];
-var cambio = ['Elige moneda de cambio','Dolar','Peso Mexicano','Peso Colombiano','Euros','Bolivar'];
-
-moneda.forEach((data,index)=>{
-     const item = document.createElement('option');
-     item.setAttribute('value',index)
-     item.textContent = data;
-     fragmento.appendChild(item)   
-})
-
-cambio.forEach((data,index)=>{
-     const item = document.createElement('option');
-     item.setAttribute('value',index)
-     item.textContent = data;
-     fragmento2.appendChild(item)   
-})
-
-listaMoneda.appendChild(fragmento);
-listaCambio.appendChild(fragmento2);
-
-formulario.addEventListener('submit', (e) => {
-     e.preventDefault();
-
-     // leer la moneda seleccionada
-     const monedaSelect = document.getElementById('moneda');
-     const monedaSeleccionada = monedaSelect.options[monedaSelect.selectedIndex].value;
-   
-      console.log(monedaSeleccionada);
-
-     // leer la criptomoneda seleccionada
-     const criptoMonedaSelect = document.getElementById('criptomoneda');
-     const criptoMonedaSeleccionada = criptoMonedaSelect.options[criptoMonedaSelect.selectedIndex].value;
- 
-     const divMensaje = document.createElement('div');
-     divMensaje.classList.add('text-center', 'alert');
-
-     // comprobar que ambos campos tengan algo seleccionado
-     if(monedaSeleccionada == 0 || criptoMonedaSeleccionada == 0) {
-          divMensaje.classList.add('alert-danger');
-          divMensaje.appendChild(document.createTextNode('Es requisito escoger las monedas'));
-          alerta.appendChild(divMensaje)
-     } else {
-          divMensaje.classList.add('alert-success');
-          divMensaje.appendChild(document.createTextNode('Gracias'));
-          alerta.appendChild(divMensaje)
+     constructor(dollaValue, currencyName){
+          this.dollaValue = dollaValue;
+          this.currencyName = currencyName;
      }
 
+     /* Amount */
+     set amount(newAmount){
+          newAmount = parseFloat(newAmount);
+          if (isNaN(newAmount)) throw 'Invalid Amount'
+          this.#amount = newAmount;
+     }
+
+     get amount(){
+          return this.#amount;
+     }
+
+     /* Dollar Value */
+     set dollaValue(newValue){
+          newValue = parseFloat(newValue);
+          if (isNaN(newValue)) throw 'Invalid Value'
+          this.#dollarValue = newValue;
+     }
+
+     get dollaValue(){
+          return this.#dollarValue
+     }
+
+     /* Currency Name */
+     get currencyName(){
+          return this.#name;
+     }
+
+     set currencyName(newName){
+          if(typeof(newName) !== 'string' || newName==='')throw 'Invalid currency name';
+          this.#name = newName
+     }
+
+     transformCurrency(currency){
+          return (this.#dollarValue * this.#amount) / currency.dollaValue;
+     }
+}
+
+/* Currencies */
+//Vector Moneda
+const Currencies = [
+     new Currency(1,'Dolar'),
+     new Currency(0.055,'Peso Mexicano'),
+     new Currency(0.00024,'Peso Colombiano'),
+     new Currency(1.07, 'Euros'),
+     new Currency(0.028,'Bolivar Venezolano')
+];
+const Criptos= [
+     new Currency(63671.557, 'Bitcoin'),
+     new Currency(3421.4681, 'Ethereum'),
+     new Currency(584.87, 'BNB'),
+     new Currency(131.76, 'Solana'),
+     new Currency(1, 'Tether'),
+];
+
+const AddOptionsToSelect = ({selectId, currencies = []})=>{
+     const Template = document.createDocumentFragment();
+     const Select = document.querySelector(`#${selectId}`);
+     const DefaultOption = document.createElement('option');
+
+     /* Set default option */
+     DefaultOption.setAttribute('value', '-')
+     DefaultOption.setAttribute('selected', '')
+     DefaultOption.textContent = '-- Selecciona una Divisa --';
+
+     /* Add default option to template */
+     Template.appendChild(DefaultOption);
+
+     /* Add currencies to template */     
+     currencies.forEach((currency, i)=>{
+          const Option = document.createElement('option');
+          /* Set properties */
+          Option.setAttribute('value',i);
+          Option.textContent = currency.currencyName;
+          /* add Option */
+          Template.appendChild(Option)   
+     })
+     /* add to template */
+     Select.append(Template.cloneNode(true));     
+}
+
+const AddMessage = ({isPositiveMessage = false, message = 'Gracias'})=>{
+     const Alert = document.createElement('div');
+     const Message = document.createTextNode(message);
+     const GetClass = (isSuccess) => (isSuccess) ? 'alert-success': 'alert-danger';
+     
+     /* Set atributtributes */
+     Alert.classList.add('text-center', 'alert');
+     Alert.classList.add(GetClass(isPositiveMessage));
+     Alert.classList.remove(GetClass(!isPositiveMessage));
+
+     /* Append Elemnts */
+     Alert.appendChild(Message);
+     document.querySelector('#principal').appendChild(Alert);
+
+     /* Add Events */
      setTimeout(function() {
           document.querySelector('#principal .alert').remove();
-          formulario.reset();
      }, 3000);
+}
 
-
-     console.log(" Moneda selecc" +criptoMonedaSeleccionada)
-     
-     switch (criptoMonedaSeleccionada) {
-          case "1":
-               console.log('Dolares')
-               break;
-          case "2":
-               console.log('Peso Mexicano ')
-               break;
-          case "3":
-               console.log('Pesos Colombiano ')
-               break;
-          default:
-               console.log('opción invalida ')
-               break;
+const CalculateCurrency = (e)=>{
+     e.preventDefault();
+     const Form = e.target;
+     const Value = Form.amount.value;
+     const CurrencyIndex = Form.moneda.value; 
+     const CriptoIndex = Form.criptomoneda.value;      
+     const Currency = Currencies[CurrencyIndex];
+     const Cripto = Criptos[CriptoIndex];
+     /* Show Alert */
+     if(
+          CriptoIndex === '-' || 
+          CurrencyIndex ==='-' || 
+          isNaN(CurrencyIndex) ||
+          isNaN(CriptoIndex) ||
+          ! Currency ||
+          ! Cripto ||
+          Value < 0 
+     ){
+          AddMessage({message:'Debe seleccionar una divisa e ingresar una cantidad'});
+          return;
      }
 
+     Currency.amount = Value;
+     let conversion = Currency.transformCurrency(Cripto);
+
+     AddMessage({
+          isPositiveMessage:true, 
+          message: `
+               El valor de ${Currency.currencyName} a la 
+               criptomoneda ${Cripto.currencyName} 
+               es de: ${conversion.toFixed(10)}`
+     })
+     
+     formulario.reset();
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+     /* Add Currencies */
+     AddOptionsToSelect({
+          selectId:'moneda',
+          currencies:Currencies
+     });
+     /* Add Criptos */
+     AddOptionsToSelect({
+          selectId:'criptomoneda',
+          currencies:Criptos
+     });
+
+     /* Add form event */
+     document.querySelector('#formulario')
+          .addEventListener('submit', CalculateCurrency);
 })
